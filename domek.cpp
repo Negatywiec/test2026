a72c6b77d526fc6a711b8cd30eed156a728d75a3
Negatywiec <l.cylejowski@gmail.com>
2026-08-03 21:43:28 +0200

    Implement random number generator in domek.cpp
    Added random number generator function and updated main.

--- domek.cpp
+++ domek.cpp
@@ -1,1 +1,20 @@
-#include "domek.h"
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace std;
+int randomizer(int a, int b) {
+  random_device rd;
+  mt19973 engine(rd());
+  uniform_int_distribution<int>ran(a, b);
+  return ran(engine);
+}
+
+
+int main() {
+  setlocale(LC_CTYPE, "Polish");
+
+  cout << randomizer(2, 8) << "fin";
+  
+  return 0;
+}
